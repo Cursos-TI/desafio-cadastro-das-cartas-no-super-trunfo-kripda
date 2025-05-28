bad4ea3a13ae52b22b92b916b42dfc34c7c0033b
kripda <mateussalesv@gmail.com>
2025-05-28 22:38:21 +0000

    dados das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,39 @@
 //Teste larissa
 
 int main() {
+    char estado;
+    char codigodacarta [20];
+    char nomedacidade [50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosturisticos;
+
+    printf("Digite o estado: ");
+    scanf("%s", estado);
+    
+    printf("Digite o codigo da carta: ");
+    scanf("%s", codigodacarta);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nomedacidade);
+
+    printf("Digite a pupulação: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a area: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB ");
+    scanf("%f", &pib);
+
+    printf("Quantidade de pontos turisticos ");
+    scanf("%d", &pontosturisticos);
+
+    printf("carta1: \n");
+    printf()
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
